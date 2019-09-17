b085b9cf25b722ef026a2f4b9883df2f345c823e
Yile.Shi <1000839@wifi-staff-172-24-13-46.net.auckland.ac.nz>
2019-09-18 11:09:56 +1200

    a

--- src/TrainModel/test.py
+++ src/TrainModel/test.py
@@ -6,8 +6,9 @@ path = '../../data/Study3_raw_data/'
 files = os.listdir(path)
 
 for file in files:
-	data = pd.read_csv(path+file)
-	data['mx'] = data['ax']
-	data['my'] = data['ay']
-	data['mz'] = data['az']
-	data.to_csv(path+file)
+	if '.csv' in file:
+		data = pd.read_csv(path+file)
+		data['mx'] = data['ax']
+		data['my'] = data['ay']
+		data['mz'] = data['az']
+		data.to_csv(path+file)
