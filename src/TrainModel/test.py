db9c8018381595dedd45a0f26cdf819952d80252
Yile.Shi <1000839@wifi-staff-172-24-13-46.net.auckland.ac.nz>
2019-09-18 11:06:36 +1200

    analysis

--- src/TrainModel/test.py
+++ src/TrainModel/test.py
@@ -3,20 +3,13 @@ import os
 import pandas as pd
 import numpy.fft
 
-path = '../../data/Study5_force_derivative/'
-names = ['chamod','clarence','haimo','hussel','jiashuo','logan','sachith','samitha','shamane','tharindu','vipula','yilei']
-numbers = ['1','2','3','4','5','6']
+path = '../../data/Study3_raw_data/'
+
 files = os.listdir(path)
 
-for name in names:
-	for number in numbers:
-		print(path+name+'_high_force'+number+'.csv', path+name+'_low_force'+number+'.csv')
-		data1 = pd.read_csv(path+name+'_high_force'+number+'.csv').values
-		data2 = pd.read_csv(path+name+'_low_force'+number+'.csv').values
-		fft1 = numpy.fft.fft(data1[:,0])
-		fft2 = numpy.fft.fft(data2[:,0])
-
-		plt.plot(fft1, label='high')
-		plt.plot(fft2, label='low')
-		plt.ylabel('some numbers')
-		plt.show()
+for file in files:
+	data = pd.read_csv(path+file)
+	data['mx'] = data['ax']
+	data['my'] = data['ay']
+	data['mz'] = data['az']
+	data.to_csv(path+file)
